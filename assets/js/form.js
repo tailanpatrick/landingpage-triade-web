eeccd0fb0307bdce1ba915a4a9369ce50c3c427b
tailanpatrick <tailanpatrick06@gmail.com>
2024-02-03 15:32:53 -0300

    Formulário já enviando os dados para Whatsapp

---
+++ assets/js/form.js
@@ -0,0 +1,17 @@
+const URL_API = "https://wa.me/";
+const TEL = "5531988076914"
+
+function sendMessage(){
+    const name = document.getElementById('name').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    let fullMessage = `Oi, sou ${name}, e estou entrando em contato pelo formulário da sua página ${document.title}, \n\n`;
+    fullMessage += message;
+
+    const encode = encodeURIComponent(fullMessage);
+
+    const finalURL = `${URL_API}${TEL}?text=${encode}`;
+
+    window.open(finalURL, '_blank');
+}
+
